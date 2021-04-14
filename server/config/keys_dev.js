3dc7a222c23fed363272a609187721f07ae49b93
KosSid <54644828+KosSid@users.noreply.github.com>
2021-04-14 11:40:10 +0300

    setup commit #1

---
+++ server/config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:'mongodb+srv://ksydorenko:dasha123@cluster0.qcbvx.gcp.mongodb.net/boring_weekend?retryWrites=true&w=majority',
+  secretOrKey: 'secretKey123456789',
+};
