5967b6c6ed55bf153a8cd8d3a4895939d1c85016
KosSid <54644828+KosSid@users.noreply.github.com>
2021-04-17 02:25:35 +0300

    home page basis

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import homeImg from '../../images/Home_img/Rectangle 5 (3).png'
+import homeImg from '../../images/Home_img/home_img.png'
 
 import './style.scss'
 
 const Home = () => {
     return (
         <div className={'home__container'}>
-            <img className={'img__page'} src={homeImg} alt='home_img'/>
+            <div className={'home__card'}>
+                <img className={'home__img__page'} src={homeImg} alt='home_img'/>
+                <div className={'home__circle__container'}>
+                    <div className={'home__circle'}>icon</div>
+                    <div className={'home__circle'}>icon</div>
+                    <div className={'home__circle'}>icon</div>
+                </div>
+            </div>
+
         </div>
     );
 };
