f387b228da932cef8f24b5a2cd73f03da47a73df
KosSid <54644828+KosSid@users.noreply.github.com>
2021-04-22 00:10:25 +0300

    home page styles

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import homeImg from '../../images/Home_img/home_img.png'
+import homeImg from '../../sources/images/Home_img/home_img.png';
+import { AiFillHeart, AiOutlineClose, AiOutlineQuestion } from "react-icons/ai";
 
 import './style.scss'
 
@@ -9,9 +10,27 @@ const Home = () => {
             <div className={'home__card'}>
                 <img className={'home__img__page'} src={homeImg} alt='home_img'/>
                 <div className={'home__circle__container'}>
-                    <div className={'home__circle'}>icon</div>
-                    <div className={'home__circle'}>icon</div>
-                    <div className={'home__circle'}>icon</div>
+                    <div className={'home__circle'}>
+                        <AiOutlineClose className={'Close-icon'}/>
+                    </div>
+                    <div className={'home__circle'}>
+                        <AiOutlineQuestion className={'Maybe-icon'}/>
+                    </div>
+                    <div className={'home__circle'}>
+                        <AiFillHeart className={'Heart-icon'}/>
+                    </div>
+                </div>
+            </div>
+            <div className={'home__message'}>
+                <div className={'home__message'}>
+                    <h2>Do you like?</h2>
+                    <h1><strong>SIGHTSEEING</strong></h1>
+                    <div>
+                        <p>Arhitecture</p>
+                        <p>Culture</p>
+                        <p>Parks</p>
+                    </div>
+
                 </div>
             </div>
 
